149a37dc0ac28614a3c4424636df42cf4c04c728
gloria bello <calderonscarlette69@gmail.com>
2022-09-27 14:45:02 -0300

    ciclo 2 27 sep

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+
+// contar desde 10 a 1
+number = 10
+
+do {
+    console.log(number)
+    number--
+}while(number >= 1)
+
+//mostrar un nombre al revés
+
+const name = "gloria"
+let index = name.length -1
+do {
+console.log(name[index])
+index-- 
+} while(index >= 0)
